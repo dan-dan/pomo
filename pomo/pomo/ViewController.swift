4b281894161d097b5bb8ca3c7fa97a1dca090bb1
dan-dan <danielhc2@gmail.com>
2016-09-18 19:00:13 -0700

    Initial commit

---
+++ pomo/pomo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  pomo
+//
+//  Created by Daniel Cortez on 9/18/16.
+//  Copyright © 2016 HCD. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
